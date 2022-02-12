f34f15f906509f40e9f86c04a85c8ee1fe99d21d
wasshoy <404ari@gmail.com>
2022-02-12 09:32:15 +0900

    🎨  ESLint と Prettier 導入

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+    'prettier',  // その他の設定を上書きするために最後に配置
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+  },
+};
